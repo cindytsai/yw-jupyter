6f5847bcf3f3dd33318b0430ae97360c67666ee8
Shin-Rong Tsai <turquoisea.tsai@gmail.com>
2025-10-02 13:49:24 -0500

    call yw-core

--- src/yw-core.ts
+++ src/yw-core.ts
@@ -11,14 +11,17 @@ export interface YWEdge {
 /**
  * Main API for compute the edges for the YesWorkflow visualization.
  * Must have a kernel with yw-core installed.
+ * @note currently, we rely on printing the result to stdout and then parse it.
  * @todo need to create a temporary storage for cells data, so that we don't overwrite existing Python objects
  * @param kernel kernel connection to call yw-core
  * @param input_cells list of code string in each cell
+ * @param yw_core_estimate "Upper" or "Lower" estimate, default to "Upper"
  * @returns YWEdge[] computed by yw-core
  */
 export function computeEdges(
   kernel: Kernel.IKernelConnection | undefined | null,
-  input_cells: string[]
+  input_cells: string[],
+  yw_core_estimate: 'Upper' | 'Lower' = 'Upper'
 ): YWEdge[] {
   if (!kernel) {
     Notification.error(
@@ -30,9 +33,9 @@ export function computeEdges(
   }
 
   // load input cells to Python, and create cell_list = [list of cell_i]
-  let py_cell_list = "";
+  let py_cell_list = '';
   input_cells.forEach((cell, index) => {
-    const py_cell = `cell_${index} = """${cell}"""\n`;  // TODO: might encounter issues if triple quotes are in the code
+    const py_cell = `cell_${index} = """${cell}"""\n`; // TODO: might encounter issues if triple quotes are in the code
     py_cell_list += `cell_${index},`;
     kernel.requestExecute({
       code: py_cell,
@@ -41,24 +44,25 @@ export function computeEdges(
     });
   });
   kernel.requestExecute({
-    code: "cell_list = [" + py_cell_list + "]\n",
+    code: 'cell_list = [' + py_cell_list + ']\n',
     silent: false,
     store_history: false
   });
 
   // call yw-core to compute edges silently
-  const test_code =
-    "import ast, sys\nast.parse('a=1')\na=1\n" +
-    '\nprint(sys.version)\na';
+  const is_upper_estimate = yw_core_estimate === 'Upper' ? 'True' : 'False';
+  const py_yw_core =
+    `from yw_core.yw_core import extract_records\n` +
+    `print(extract_records(cell_list, is_upper_estimate=${is_upper_estimate}))\n`;
   const exec_result = kernel.requestExecute({
-    code: test_code,
+    code: py_yw_core,
     silent: false,
     store_history: false
   });
   console.log('[computeEdges] ', exec_result);
   let output: string | string[] | null | undefined = null;
   exec_result.onIOPub = (msg: KernelMessage.IIOPubMessage) => {
-    if (msg.header.msg_type === 'stream') { // TODO: how to make yw-core show the output? (https://jupyterlab.readthedocs.io/en/3.4.x/api/modules/nbformat.html#multilinestring)
+    if (msg.header.msg_type === 'stream') {
       const content = msg.content as IStream;
       output = content.text;
       console.log('[computeEdges] stream output: ', output);
@@ -74,7 +78,7 @@ export function computeEdges(
     });
   });
   kernel.requestExecute({
-    code: "del(cell_list)\n",
+    code: 'del(cell_list)\n',
     silent: false,
     store_history: false
   });
@@ -87,6 +91,8 @@ export function computeEdges(
  * // TODO: compute the edges using yw-core
  * @param output
  */
-function parseYWCoreOutput(output: string | string[] | null | undefined): YWEdge[] {
+function parseYWCoreOutput(
+  output: string | string[] | null | undefined
+): YWEdge[] {
   return [{ id: 'e0-1', source: '0', target: '1' }];
 }
