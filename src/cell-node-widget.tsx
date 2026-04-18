a6fa04a045d7f1cca2aa906f9c87684bda3f7386
Shin-Rong Tsai <turquoisea.tsai@gmail.com>
2026-04-17 19:37:30 -0500

    Add status side bar.

--- src/cell-node-widget.tsx
+++ src/cell-node-widget.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, memo } from 'react';
 
 import { BaseNode } from './components/base-node';
+import { NodeSidePanel } from './components/node-side-panel';
 import {
   NodeHeader,
   NodeHeaderTitle,
@@ -23,38 +24,47 @@ export interface ICellNodeData extends Record<string, unknown> {
   status: 'not-execute' | 'executing' | 'executed';
 }
 
+const CELL_NODE_STATUS_COLOR: Record<string, string> = {
+  Executed: '#0052B0',
+  NotExecute: '#C2C2C2',
+  Editing: '#EC7500'
+};
+
 export type CellNode = Node<ICellNodeData>;
 
 export const CellNodeWidget = memo(
   ({ data, selected }: NodeProps<CellNode>) => {
     return (
-      <BaseNode selected={selected} className="px-3 py-2">
-        <NodeHeader className="-mx-3 -mt-2 border-b">
-          <NodeHeaderTitle>{data.header}</NodeHeaderTitle>
-          <NodeHeaderActions>
-            {/*<NodeHeaderDiveInAction />*/}
-            <NodeHeaderRunAction />
-            <NodeHeaderRunAllDownstreamAction />
-            <NodeHeaderExportAction />
-            {/*  <NodeHeaderDeleteAction />*/}
-          </NodeHeaderActions>
-        </NodeHeader>
-        <div className="mt-2">
-          <CodeEditor
-            value={data.code_block}
-            language="python"
-            data-color-mode="light"
-            onChange={data.on_content_change}
-            style={{
-              backgroundColor: '#f5f5f5',
-              fontFamily:
-                'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace'
-            }}
-          />
-        </div>
-        <Handle type="target" position={Position.Top} />
-        <Handle type="source" position={Position.Bottom} />
-      </BaseNode>
+      <>
+        <BaseNode selected={selected} className="px-3 py-2">
+          <NodeSidePanel color={CELL_NODE_STATUS_COLOR['NotExecute']} />
+          <NodeHeader className="-mx-3 -mt-2 border-b">
+            <NodeHeaderTitle>{data.header}</NodeHeaderTitle>
+            <NodeHeaderActions>
+              {/*<NodeHeaderDiveInAction />*/}
+              <NodeHeaderRunAction />
+              <NodeHeaderRunAllDownstreamAction />
+              <NodeHeaderExportAction />
+              {/*  <NodeHeaderDeleteAction />*/}
+            </NodeHeaderActions>
+          </NodeHeader>
+          <div className="mt-2">
+            <CodeEditor
+              value={data.code_block}
+              language="python"
+              data-color-mode="light"
+              onChange={data.on_content_change}
+              style={{
+                backgroundColor: '#f5f5f5',
+                fontFamily:
+                  'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace'
+              }}
+            />
+          </div>
+          <Handle type="target" position={Position.Top} />
+          <Handle type="source" position={Position.Bottom} />
+        </BaseNode>
+      </>
     );
   }
 );
